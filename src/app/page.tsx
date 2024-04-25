d34288993195acea56eb93d6d4b2766c4ddf22f2
KSiig <kasper@siig.tech>
2024-04-25 17:27:50 +0200

    Changed header text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Velkommen til vores&nbsp;
+          <code className={styles.code}>madplan</code>
         </p>
         <div>
           <a
